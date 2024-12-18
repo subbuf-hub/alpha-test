3dab0a3e688f779d17501b023d33f97c0d8d960b
i_polugrudov <ipolugrudov@gmail.com>
2024-12-18 10:14:43 +0300

    все

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
-import { Link, Route, BrowserRouter , Routes } from 'react-router-dom'
-import './App.css'
-import HeroesPage from './pages/HeroesPage'
-import HeroDetail from './pages/HeroDetail '
-import CreateHeroe from './pages/CreateHeroe'
+import { Link, Route, HashRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import HeroesPage from "./pages/HeroesPage";
+import HeroDetail from "./pages/HeroDetail ";
+import CreateHeroe from "./pages/CreateHeroe";
 
 function App() {
-
   return (
     <div>
-      <BrowserRouter >
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link className="navbar-brand" to="/">
-            Альфа тест
-          </Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/heroes">
-                  Герои
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/create-heroe">
-                  Создать героя
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HeroesPage />} />
-          <Route path="/heroes" element={<HeroesPage />} />
-          <Route path="/heroes/:id" element={<HeroDetail  />} />
-          <Route path="/create-heroe" element={<CreateHeroe />} />
-        </Routes>
-      </div>
-    </BrowserRouter >
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+              Альфа тест
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/heroes">
+                    Герои
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/create-heroe">
+                    Создать героя
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Routes>
+            <Route path="/" element={<HeroesPage />} />
+            <Route path="/heroes" element={<HeroesPage />} />
+            <Route path="/heroes/:id" element={<HeroDetail />} />
+            <Route path="/create-heroe" element={<CreateHeroe />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
